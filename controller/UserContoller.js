60a05085ca639e334a66d13d87269273b1fe2126
Albertino Vieira <8140346@estg.ipp.pt>
2023-03-11 23:09:52 +0000

    fix on deploy

--- controller/UserContoller.js
+++ controller/UserContoller.js
@@ -40,7 +40,7 @@ const login = async (req, res) => {
       return res.json({ auth: true, token: token, ...userExists._doc });
     } else {
       return res.status(400).json({
-        error: "Email or password incorrect",
+        error: "Error",
       });
     }
   } else {
